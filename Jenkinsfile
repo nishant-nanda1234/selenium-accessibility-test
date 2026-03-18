515834957a40f137b6dfd2d97ae121f8b4a8ef5b
Nishant Malhotra <malhotrad58@yahoo.com>
2026-03-18 23:11:45 +0530

    Push code to selenium-accessibility-test repo

--- Jenkinsfile
+++ Jenkinsfile
@@ -9,6 +9,14 @@ pipeline {
                 bat 'mvn clean test'
             }
         }
+        stage('Applitools Visual Test') {
+            steps {
+                // Set Applitools API key as environment variable (replace with your actual key or use Jenkins credentials)
+                withEnv(['APPLITOOLS_API_KEY=your_applitools_api_key']) {
+                    bat 'mvn -Dtest=SampleUiTest test'
+                }
+            }
+        }
         stage('Allure Report') {
             steps {
                 allure includeProperties: false, jdk: '', results: [[path: 'allure-results']]
--- src/test/java/tests/SampleUiTest.java
+++ src/test/java/tests/SampleUiTest.java
@@ -8,24 +8,52 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.applitools.eyes.selenium.Eyes;
+import com.applitools.eyes.RectangleSize;
+import com.applitools.eyes.selenium.EyesRunner;
+import com.applitools.eyes.selenium.ClassicRunner;
+import com.applitools.eyes.TestResults;
+import com.applitools.eyes.BatchInfo;
+import com.applitools.eyes.selenium.EyesException;
+
 public class SampleUiTest {
     private WebDriver driver;
+    private EyesRunner runner;
+    private Eyes eyes;
 
     @BeforeMethod
     public void setUp() {
         driver = DriverFactory.getDriver();
+        runner = new ClassicRunner();
+        eyes = new Eyes(runner);
+        eyes.setApiKey(System.getenv("APPLITOOLS_API_KEY")); // Set your Applitools API key as env variable
+        eyes.setBatch(new BatchInfo("UI Visual Tests"));
     }
 
     @Test
     public void openGoogleTest() {
         driver.get("https://www.google.com");
+        // Start visual test
+        eyes.open(driver, "Google", "Google Homepage Test", new RectangleSize(1024, 768));
+        // Visual checkpoint
+        eyes.checkWindow("Google Homepage");
+        // Usual assertion
         Assert.assertTrue(driver.getTitle().contains("Google"));
+        // End visual test
+        eyes.closeAsync();
     }
 
     @AfterMethod
     public void tearDown() {
         if (driver != null) {
             driver.quit();
+        }
+        if (eyes != null) {
+            try {
+                eyes.abortIfNotClosed();
+            } catch (EyesException e) {
+                // Ignore if already closed
+            }
         }
     }
 }
